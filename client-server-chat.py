3548d43d2212046cdd315697048b1129dfe8ded2
jrussell-OSU <russelj2@oregonstate.edu>
2021-11-25 09:07:44 -0500

    server and client chat working. now attempting to do extra credit ASCII game

--- client-server-chat.py
+++
@@ -1,8 +0,0 @@
-# Author: Jacob Russell
-# Class: CS372 Networking
-# Date: November 23, 2021
-# Description: A client-server chat program using a python socket
-# Works Cited: https://docs.python.org/3.4/howto/sockets.html and https://realpython.com/python-sockets/
-from socket import *
-
-
--- server_game.py
+++ server_game.py
@@ -7,21 +7,6 @@
 from socket import *
 import select
 
-# Server side instructions:
-
-"""
-Server
-1. The server creates a socket and binds to ‘localhost’ and port xxxx
-2. The server then listens for a connection
-3. When connected, the server calls recv to receive data
-4. The server prints the data, then prompts for a reply
-5. If the reply is /q, the server quits
-6. Otherwise, the server sends the reply
-7. Back to step 3
-8. Sockets are closed (can use with in python3)
-"""
-
-
 # Create server socket, bind to localhost on specified port
 server_host = 'localhost'
 server_port = 9998
